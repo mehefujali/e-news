0358cd9a15d3bbc6d37f674e6c1703135d5be3c1
Mehefuj Ali <mehefujalim@gmail.com>
2024-11-15 01:46:55 +0530

    news details and privet route created

--- src/Auth/Private/PrivetRoute.jsx
+++ src/Auth/Private/PrivetRoute.jsx
@@ -1,11 +1,18 @@
+import { useContext } from "react";
+import { AuthContext } from "../../Context/AuthProvider";
+import { Navigate } from "react-router-dom";
+import PropTypes from "prop-types";
 
 
-const PrivetRoute = () => {
-      return (
-            <div>
-                  
-            </div>
-      );
+const PrivetRoute = ({children}) => {
+      const {user} = useContext(AuthContext)
+
+      if(user){
+            return  children
+      }
+      return <Navigate to='/login' ></Navigate>
 };
-
+PrivetRoute.propTypes={
+      children:PropTypes.object.isRequired
+}
 export default PrivetRoute;
--- src/Pages/NewsDetails/NewsDetails.jsx
+++ src/Pages/NewsDetails/NewsDetails.jsx
@@ -1,16 +1,49 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, ScrollRestoration, useLoaderData } from "react-router-dom";
+import Header from "../../Components/Header/Header";
+import AsideRight from "../../Components/AsideRight/AsideRight";
+import { FaArrowLeft } from "react-icons/fa6";
+import PrivetRoute from "../../Auth/Private/PrivetRoute";
 
 
 const NewsDetails = () => {
       const data = useLoaderData()
       const news = data.data[0]
-   
-      
+
+      const {
+
+
+            details,
+            image_url,
+
+
+            title,
+
+            category_id
+      } = news;
 
       return (
-            <div>
+            <PrivetRoute>
+                  <div className=" container mx-auto">
+                        <ScrollRestoration></ScrollRestoration>
+                        <Header></Header>
+                        <div className=" grid grid-cols-12 gap-7  ">
+                              <div className=" col-span-9">
+                                    <h1 className=" text-xl font-bold mt-8">E News</h1>
+                                    <div className=" p-8 mt-4 border-2 border-blue-200 rounded">
+                                          <img src={image_url} alt="" />
+                                          <h1 className=" text-2xl font-bold my-2">{title}</h1>
+                                          <p>{details}</p>
+                                          <Link to={`/home/category/${category_id}`} className=" flex btn  bg-blue-600 text-white hover:text-blue-600 w-fit mt-4 rounded"><FaArrowLeft></FaArrowLeft> All news in this category</Link>
+                                    </div>
 
-            </div>
+                              </div>
+                              <div className=" col-span-3">
+                                    <AsideRight></AsideRight>
+                              </div>
+
+                        </div>
+                  </div>
+            </PrivetRoute>
       );
 };
 
