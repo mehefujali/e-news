2eed73fd1ab3d094ac4d6fd0d0d53192b4f47917
Mehefuj Ali <mehefujalim@gmail.com>
2024-11-15 00:39:02 +0530

    conditional socal login created

--- src/Auth/SocalLogin/SocalLogin.jsx
+++ src/Auth/SocalLogin/SocalLogin.jsx
@@ -1,12 +1,19 @@
+import { useContext } from "react";
 import { FaFacebook, FaGoogle } from "react-icons/fa";
+import { AuthContext } from "../../Context/AuthProvider";
 
 
 const SocalLogin = () => {
+      const {googleLogIn,setUser} = useContext(AuthContext)
+      const handleGoogleLogin = ()=> {
+            googleLogIn()
+            .then(res=> setUser(res.user))
+      }
       return (
             <div>
                   <h2 className=" text-sm xl:text-xl font-bold">Login with</h2>
                   <div className=" mt-4 space-y-2  flex flex-col">
-                        <button className=" btn  btn-outline xl:text-lg gap-2 flex  items-center" ><FaGoogle className=" text-2xl"></FaGoogle> <span>Login with Google</span></button>
+                        <button onClick={handleGoogleLogin} className=" btn  btn-outline xl:text-lg gap-2 flex  items-center" ><FaGoogle className=" text-2xl"></FaGoogle> <span>Login with Google</span></button>
                         <button className="btn text-blue-500 btn-outline  xl:text-lg gap-2 flex  items-center" ><FaFacebook className=" text-2xl"></FaFacebook> <span >Login with Facebook</span></button>
                         
                   </div>
--- src/Components/AsideRight/AsideRight.jsx
+++ src/Components/AsideRight/AsideRight.jsx
@@ -1,12 +1,17 @@
+import { useContext } from "react";
 import SocalLogin from "../../Auth/SocalLogin/SocalLogin";
 import FindUsSocal from "../FindUsSocal/FindUsSocal";
 import Qzone from "../Qzone/Qzone";
+import { AuthContext } from "../../Context/AuthProvider";
 
 
 const AsideRight = () => {
+      const {user} = useContext(AuthContext)
       return (
             <div className=" px-4 md:px-0">
-               <SocalLogin></SocalLogin>  
+               {
+                  user? '' : <SocalLogin></SocalLogin> 
+               } 
                <div className=" mt-8">
                <FindUsSocal></FindUsSocal>   
                </div>
