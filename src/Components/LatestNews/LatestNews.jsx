112cecec41db6c317d40d93bc1037ad0dd06c134
Mehefuj Ali <mehefujalim@gmail.com>
2024-11-16 10:48:23 +0530

    responsive latest news

--- src/Components/LatestNews/LatestNews.jsx
+++ src/Components/LatestNews/LatestNews.jsx
@@ -19,10 +19,10 @@ const LatestNews = () => {
        
       return (
             <div className=" container mx-auto mt-3">
-                 <div className=" flex items-center bg-blue-200 p-2">
-                   <span className=" bg-blue-600 text-white px-6 py-3">Trending</span> <Marquee 
+                 <div className=" flex items-center bg-blue-200  w-full">
+                   <span className=" bg-blue-600 text-white px-3 py-1 md:px-6 md:py-3">Trending</span> <Marquee 
                    pauseOnHover={true}>
-                        <div className=" space-x-8 text-xl">
+                        <div className=" space-x-8 text-sm md:text-xl">
                              {
                               latestNews.map(news=> <Link className="" to={`/newsdetails/${news._id}`} key={news._id}> <span className=" text-blue-600">#</span><span>{news.title}</span></Link>)
                              }
